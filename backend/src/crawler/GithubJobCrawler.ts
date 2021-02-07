1707197b5ed864127d1b8217dba0924d4c463d94
Tiago rosa da costa <tiagorosadacost@gmail.com>
2021-02-06 22:23:05 -0300

    Refactory in class GithubJobCrawler remove duplicate code

--- backend/src/crawler/GithubJobCrawler.ts
+++ backend/src/crawler/GithubJobCrawler.ts
@@ -5,54 +5,38 @@ import moment from "moment";
 
 export default class GithubJobCrawler implements CrawlerInterface {
 
+    private extractJob(item: any) {
+        let company = (item.title.split("@")[1] || '').trim();
+        let location = item.title.split(/]/);
+        location = location[0];
+        location = location.replace("[", '')
+        location = location.replace("]", '')
+
+        return {
+            objectID: UuidUtil.get(),
+            title: item.title,
+            // @ts-ignore
+            link: item.html_url,
+            // @ts-ignore
+            location: location,
+            company: {
+                // @ts-ignore
+                logo: "",
+                // @ts-ignore
+                name: company
+            },
+            extracted_at: moment.utc()
+        }
+    }
+
     async execute(options: { [key: string]: any; }): Promise<any> {
         let frontendbrJobs = await axios.get("https://api.github.com/repos/frontendbr/vagas/issues");
         let result = await axios.get("https://api.github.com/repos/backend-br/vagas/issues");
         result = result.data.map((item: any) => {
-            let company = (item.title.split("@")[1] || '').trim();
-            let location = item.title.split(/]/);
-            location = location[0];
-            location = location.replace("[", '')
-            location = location.replace("]", '')
-
-            return {
-                objectID: UuidUtil.get(),
-                title: item.title,
-                // @ts-ignore
-                link: item.html_url,
-                // @ts-ignore
-                location: location,
-                company: {
-                    // @ts-ignore
-                    logo: "",
-                    // @ts-ignore
-                    name: company
-                },
-                extracted_at: moment.utc()
-            }
+            return this.extractJob(item);
         });
         frontendbrJobs = frontendbrJobs.data.map((item: any) => {
-            let company = (item.title.split("@")[1] || '').trim();
-            let location = item.title.split(/]/);
-            location = location[0];
-            location = location.replace("[", '')
-            location = location.replace("]", '')
-
-            return {
-                objectID: UuidUtil.get(),
-                title: item.title,
-                // @ts-ignore
-                link: item.html_url,
-                // @ts-ignore
-                location: location,
-                company: {
-                    // @ts-ignore
-                    logo: "",
-                    // @ts-ignore
-                    name: company
-                },
-                extracted_at: moment.utc()
-            }
+            return this.extractJob(item);
         });
         // @ts-ignore
         return [...result, ...frontendbrJobs];
