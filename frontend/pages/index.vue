0558d80506f34c7af7e1498d26cd3d0e97449afa
Tiago rosa da costa <tiagorosadacost@gmail.com>
2021-02-07 21:07:02 -0300

    Changes need in frontend

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -47,31 +47,38 @@ export default Vue.extend(
   },
   async fetch() {
     try {
+      // @ts-ignore
       const response = await jobService.search(this.searchJob, { hitsPerPage: 10, page: 1 });
       //  @ts-ignore
       this.jobs = response.hits.map((item) => ({ ...item }))
     } catch (err) {
+        // @ts-ignore
         this.$toast.error(this.$t("message.error"))
     }
   },
   methods: {
     async searchJobs(typedValue: string) {
       try {
+        // @ts-ignore
         this.searchJob = typedValue;
+        // @ts-ignore
         const response = await jobService.search(this.searchJob, { });
         const isNotReturn = response.hits.length == 0
         if (isNotReturn) {
+          // @ts-ignore
           this.$toast.success(this.$t("message.not_found_result_searched"));
           return;
         }
         // @ts-ignore
         this.jobs = response.hits.map((item) => ({ ...item }))
       } catch (err) {
+        // @ts-ignore
         this.$toast.error(this.$t("message.error"))
       }
     },
     async getJobs(limit: Number = 10, page: Number = 1) {
       try {
+        // @ts-ignore
         const response = await jobService.search(this.searchJob, { hitsPerPage: limit, page: page });
         const isNotReturn = response.hits.length == 0
         if (isNotReturn) {
@@ -82,15 +89,21 @@ export default Vue.extend(
         // @ts-ignore
         this.jobs = this.jobs.concat(...itens)
       } catch (err) {
+        // @ts-ignore
         this.$toast.error(this.$t("message.error"))
       }
     },
-
+    // @ts-ignore
     async loadMoreJobs() {
+      // @ts-ignore
       this.isLoadMore = true
+      // @ts-ignore
       const page = this.totalJobs / 10
-      await this.getJobs(10, page)
+      // @ts-ignore
+      await this.getJobs(10, page);
+      // @ts-ignore
       this.totalJobs += 10
+      // @ts-ignore
       setTimeout(() => (this.isLoadMore = false), 1000)
     },
   },
